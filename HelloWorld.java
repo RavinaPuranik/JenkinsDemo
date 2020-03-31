15c7910f76786582c822552afc4565232284c943
RavinaPuranik <ravinapuranik1@gmail.com>
2020-03-31 20:47:44 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,6 @@ class HelloWorld{
 
 public static void main(String args[]){
 System.out.println("Hello World..");
-//Welcome to Jenkins Training.
+//This is just a demo program for jenkins integration.
 }
 }
