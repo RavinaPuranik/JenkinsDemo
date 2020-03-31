3ffbbd0deccb33248faf793b7f901d415e0bf659
RavinaPuranik <ravinapuranik1@gmail.com>
2020-04-01 01:20:47 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,7 @@
 class HelloWorld{
 
 public static void main(String args[]){
-System.out.println("*****Hello World..Are you learning Jenkins??*****");
+System.out.println("*****Hello World..Are you learning Jenkins??We are using webhooks here*****");
 //This is just a demo program for jenkins integration.
 }
 }
